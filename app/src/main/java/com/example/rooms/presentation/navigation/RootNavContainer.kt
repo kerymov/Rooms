7ad6f1654d444cbd996685fcb1642293f1ce10ae
illia kerymov <illya.kerymov31@gmail.com>
2025-01-30 23:34:05 +0200

    #32. Refactor navigation.

--- app/src/main/java/com/example/rooms/presentation/navigation/RootNavContainer.kt
+++ app/src/main/java/com/example/rooms/presentation/navigation/RootNavContainer.kt
@@ -2,74 +2,111 @@ package com.example.rooms.presentation.navigation
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Groups
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import com.example.rooms.R
 import com.example.rooms.domain.repository.AccountRepository
 import com.example.rooms.domain.repository.RoomRepository
 import com.example.rooms.domain.repository.RoomsRepository
 import com.example.rooms.presentation.components.BottomNavigationBar
-import com.example.rooms.presentation.components.BottomNavigationItem
+import com.example.rooms.presentation.components.CenterAlignedTopBar
+import com.example.rooms.presentation.components.TopAppBarInteractionItem
+import com.example.rooms.presentation.components.TopAppBarItem
 import com.example.rooms.presentation.features.auth.screens.SignInScreen
 import com.example.rooms.presentation.features.auth.screens.SignUpScreen
 import com.example.rooms.presentation.features.auth.viewModels.AuthViewModel
 import com.example.rooms.presentation.features.main.profile.screens.ProfileScreen
 import com.example.rooms.presentation.features.main.profile.viewModels.ProfileViewModel
 import com.example.rooms.presentation.features.main.rooms.models.RoomDetailsUi
+import com.example.rooms.presentation.features.main.rooms.screens.RoomsScreen
+import com.example.rooms.presentation.features.main.rooms.viewModels.RoomsViewModel
 import com.example.rooms.presentation.features.room.screens.ResultsScreen
 import com.example.rooms.presentation.features.room.screens.RoomScreen
-import com.example.rooms.presentation.features.main.rooms.screens.RoomsScreen
 import com.example.rooms.presentation.features.room.viewModels.RoomViewModel
-import com.example.rooms.presentation.features.main.rooms.viewModels.RoomsViewModel
 import com.example.rooms.presentation.features.utils.sharedViewModel
 import com.example.rooms.presentation.features.utils.toOuterScaffoldPadding
-import com.example.rooms.presentation.navigation.Main.Rooms
 import kotlinx.serialization.json.Json
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RootNavContainer(
     startNavModule: NavModule,
+    rootViewModel: RootViewModel,
     accountRepository: AccountRepository,
     roomsRepository: RoomsRepository,
     roomRepository: RoomRepository,
 ) {
     val navController = rememberNavController()
-    var currentNavModule by remember { mutableStateOf(startNavModule) }
+    val currentBackStackEntry by navController.currentBackStackEntryAsState()
+
+    val scaffoldState by rootViewModel.scaffoldState.collectAsState()
+    val topAppBarScrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
     Scaffold(
         bottomBar = {
-            if (currentNavModule == Main) {
+            val shouldShowBottomNavigation = currentBackStackEntry?.destination?.hierarchy?.any {
+                it.hasRoute(Main::class)
+            } == true
+
+            if (shouldShowBottomNavigation) {
+                val bottomNavItems = BottomNavigationItem.entries
+
                 BottomNavigationBar(
-                    items = BottomNavigationItem.entries,
+                    items = bottomNavItems,
                     onNavItemClick = { item ->
-                        navController.navigate(route = item.destination)
+                        navController.navigate(route = item.route)
                     }
                 )
             }
         },
+        topBar = {
+            scaffoldState.topAppBar?.let {
+                CenterAlignedTopBar(
+                    item = it,
+                    scrollBehaviour = topAppBarScrollBehavior
+                )
+            }
+        },
+        containerColor = MaterialTheme.colorScheme.background,
+        contentColor = contentColorFor(MaterialTheme.colorScheme.background),
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(topAppBarScrollBehavior.nestedScrollConnection)
     ) { contentPadding ->
         NavHost(
             navController = navController,
             startDestination = startNavModule,
             modifier = Modifier
                 .fillMaxSize()
-                .padding(contentPadding.toOuterScaffoldPadding())
+                .padding(contentPadding)
         ) {
             navigation<Auth>(
                 startDestination = Auth.SignIn
             ) {
                 composable<Auth.SignIn> { backStackEntry ->
-                    currentNavModule = Auth
+                    rootViewModel.updateTopAppBar(null)
 
                     val viewModel = backStackEntry.sharedViewModel<AuthViewModel>(
                         navController = navController,
@@ -86,7 +123,7 @@ fun RootNavContainer(
                     )
                 }
                 composable<Auth.SignUp> { backStackEntry ->
-                    currentNavModule = Auth
+                    rootViewModel.updateTopAppBar(null)
 
                     val viewModel = backStackEntry.sharedViewModel<AuthViewModel>(
                         navController = navController,
@@ -104,13 +141,23 @@ fun RootNavContainer(
                 }
             }
             navigation<Main>(
-                startDestination = Rooms
+                startDestination = Main.Rooms
             ) {
-                composable<Rooms> {
-                    currentNavModule = Main
-
+                composable<Main.Rooms> {
                     val roomsViewModel = viewModel<RoomsViewModel>(
                         factory = RoomsViewModel.createFactory(roomsRepository)
+                    )
+
+                    rootViewModel.updateTopAppBar(
+                        item = TopAppBarItem(
+                            title = stringResource(R.string.rooms),
+                            actions = listOf(
+                                TopAppBarInteractionItem(
+                                    icon = Icons.Filled.Add,
+                                    onClick = { /*roomsViewModel.openCreateRoomBottomSheet()*/ }
+                                )
+                            )
+                        )
                     )
 
                     RoomsScreen(
@@ -123,7 +170,7 @@ fun RootNavContainer(
                     )
                 }
                 composable<Main.Profile> {
-                    currentNavModule = Main
+                    rootViewModel.updateTopAppBar(null)
 
                     val profileViewModel = viewModel<ProfileViewModel>(
                         factory = ProfileViewModel.createFactory(accountRepository)
@@ -141,8 +188,6 @@ fun RootNavContainer(
             }
             navigation<Room>(startDestination = Room.RoomMain()) {
                 composable<Room.RoomMain> { backStackEntry ->
-                    currentNavModule = Room
-
                     val room: Room.RoomMain = backStackEntry.toRoute()
                     val roomDetails = room.details?.let { Json.decodeFromString<RoomDetailsUi>(it) }
 
@@ -151,13 +196,30 @@ fun RootNavContainer(
                             navController = navController,
                             factory = RoomViewModel.createFactory(details, roomRepository)
                         )
+
+                        rootViewModel.updateTopAppBar(
+                            item = TopAppBarItem(
+                                title = "${details.name} - ${details.settings.event.shortName}",
+                                navigationItem = TopAppBarInteractionItem(
+                                    icon = Icons.AutoMirrored.Filled.ExitToApp,
+                                    onClick = {
+                                        navController.navigate(Main.Rooms) {
+                                            popUpTo(0)
+                                        }
+                                    }
+                                ),
+                                actions = listOf(
+                                    TopAppBarInteractionItem(
+                                        icon = Icons.Filled.Groups,
+                                        onClick = { }
+                                    )
+                                ),
+                            )
+                        )
+
                         RoomScreen(
-                            onNavigateBack = {
-                                navController.navigate(Rooms) {
-                                    popUpTo(0)
-                                }
-                            },
                             onNavigateToResults = {
+
                                 navController.navigate(Room.Results) {
 
                                 }
@@ -167,18 +229,16 @@ fun RootNavContainer(
                         )
                     }
                 }
-                composable<Room.Results> { backStackEntry ->
-                    currentNavModule = Room
-
-                    val viewModel = backStackEntry.sharedViewModel<RoomViewModel>(
-                        navController = navController
-                    )
-
-                    ResultsScreen(
-                        navController = navController,
-                        roomName = ""
-                    )
-                }
+//                composable<Room.Results> { backStackEntry ->
+//                    val viewModel = backStackEntry.sharedViewModel<RoomViewModel>(
+//                        navController = navController
+//                    )
+//
+//                    ResultsScreen(
+//                        navController = navController,
+//                        roomName = ""
+//                    )
+//                }
             }
         }
     }
---
+++ app/src/main/java/com/example/rooms/presentation/navigation/RootViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.rooms.presentation.navigation
+
+import androidx.compose.runtime.Immutable
+import androidx.lifecycle.ViewModel
+import com.example.rooms.presentation.components.TopAppBarItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+@Immutable
+data class ScaffoldState(
+    val topAppBar: TopAppBarItem? = null
+)
+
+class RootViewModel : ViewModel() {
+
+    private val _scaffoldState = MutableStateFlow(ScaffoldState())
+    val scaffoldState: StateFlow<ScaffoldState> = _scaffoldState.asStateFlow()
+
+    fun updateTopAppBar(item: TopAppBarItem?) {
+        _scaffoldState.update { state ->
+            state.copy(topAppBar = item )
+        }
+    }
+}
