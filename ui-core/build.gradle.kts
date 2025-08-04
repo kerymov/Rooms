53f587c899ab1cccb56e00abc188dd4f2092e0ef
illia kerymov <illya.kerymov31@gmail.com>
2025-08-04 11:30:39 +0300

    #37. Fix Compose Preview.

--- ui-core/build.gradle.kts
+++ ui-core/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
