00d5a35eb265471a0cb46abb2971066a3df5156a
illia kerymov <illya.kerymov31@gmail.com>
2025-08-06 15:19:45 +0300

    #37. Add new colors to the app Theme.

--- ui-core/src/main/java/com/kerymov/ui_core/theme/Color.kt
+++ ui-core/src/main/java/com/kerymov/ui_core/theme/Color.kt
@@ -7,9 +7,9 @@ val Blue40 = Color(0x6614386A)
 val BlueLight = Color(0xFF284C7E)
 val Yellow = Color(0xFFF4BD01)
 val RedError = Color(0xFF8F0000)
-val Light = Color(0xFFFAFAFA)
+val Light = Color(0xFFFFFFFF)
 val Light60 = Light.copy(alpha = 0.6f)
-val LightGray = Color(0xFFEDEDED)
+val LightGray = Color(0xFFEFF2F9)
 val Dark = Color(0xFF262626)
 val Dark80 = Dark.copy(alpha = 0.8f)
 val Dark60 = Dark.copy(alpha = 0.6f)
@@ -22,3 +22,7 @@ val WhiteCube = Color.White
 val RedCube = Color.Red
 val OrangeCube = Color(0xFFFF8A00)
 val BlackCube = Color.Black
+
+val DnfRed = Color(0xFFB40000)
+val PlusTwoYellow = Color(0xFFFFC000)
+val DoneGreen = Color(0xFF00A300)
