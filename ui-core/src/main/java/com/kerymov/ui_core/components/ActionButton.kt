1fbead7425e5fc86cd5f4097604a65f574b8e22d
illia kerymov <illya.kerymov31@gmail.com>
2025-08-06 15:18:09 +0300

    #37. Redesign ActionButton.

--- ui-core/src/main/java/com/kerymov/ui_core/components/ActionButton.kt
+++ ui-core/src/main/java/com/kerymov/ui_core/components/ActionButton.kt
@@ -1,18 +1,25 @@
 package com.kerymov.ui_core.components
 
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.kerymov.ui_core.theme.RoomsTheme
 
 @Composable
 fun ActionButton(
@@ -20,24 +27,17 @@ fun ActionButton(
     contentDescription: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    isSelected: Boolean = false
-) = IconButton(
+    contentColor: Color = MaterialTheme.colorScheme.outlineVariant,
+    containerColor: Color = MaterialTheme.colorScheme.outline
+) = Container(
+    containerColor = containerColor,
     onClick = onClick,
     modifier = modifier
-        .size(56.dp)
-        .clip(RoundedCornerShape(16.dp))
-        .background(
-            if (isSelected) {
-                MaterialTheme.colorScheme.onPrimary
-            } else {
-                MaterialTheme.colorScheme.surfaceTint
-            }
-        )
 ) {
     Icon(
         imageVector = ImageVector.vectorResource(icon),
         contentDescription = contentDescription,
-        tint = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onPrimary,
+        tint = contentColor,
         modifier = Modifier.size(32.dp)
     )
 }
@@ -48,24 +48,45 @@ fun ActionButton(
     contentDescription: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    isSelected: Boolean = false
-) = IconButton(
+    contentColor: Color = MaterialTheme.colorScheme.outlineVariant,
+    containerColor: Color = MaterialTheme.colorScheme.outline
+) = Container(
+    containerColor = containerColor,
     onClick = onClick,
     modifier = modifier
-        .size(56.dp)
-        .clip(RoundedCornerShape(16.dp))
-        .background(
-            if (isSelected) {
-                MaterialTheme.colorScheme.onPrimary
-            } else {
-                MaterialTheme.colorScheme.surfaceTint
-            }
-        )
 ) {
     Icon(
         imageVector = icon,
         contentDescription = contentDescription,
-        tint = if (isSelected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onPrimary,
+        tint = contentColor,
         modifier = Modifier.size(32.dp)
     )
+}
+
+@Composable
+private fun Container(
+    containerColor: Color,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit,
+) = IconButton(
+    onClick = onClick,
+    modifier = modifier
+        .size(48.dp)
+        .clip(RoundedCornerShape(12.dp))
+        .background(containerColor)
+) {
+    content()
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ActionButtonPreview() {
+    RoomsTheme {
+        ActionButton(
+            icon = Icons.Default.Favorite,
+            contentDescription = "Favorite",
+            onClick = {}
+        )
+    }
 }
