ca7f3c96a12648e7180236a5c41ad35e4b64a358
illia kerymov <illya.kerymov31@gmail.com>
2025-08-17 13:38:12 +0300

    #37. Make TopAppBar hideable and change some icons.

--- app/src/main/java/com/kerymov/rooms/presentation/navigation/RootNavContainer.kt
+++ app/src/main/java/com/kerymov/rooms/presentation/navigation/RootNavContainer.kt
@@ -3,9 +3,9 @@ package com.kerymov.rooms.presentation.navigation
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ExitToApp
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Groups
+import androidx.compose.material.icons.automirrored.rounded.ExitToApp
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Timeline
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -15,6 +15,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
@@ -40,8 +42,8 @@ import com.kerymov.ui_onboarding.screens.SignUpScreen
 import com.kerymov.ui_onboarding.viewModels.AuthViewModel
 import com.kerymov.ui_profile.screens.ProfileScreen
 import com.kerymov.ui_profile.viewModels.ProfileViewModel
+import com.kerymov.ui_room.RoomScreen
 import com.kerymov.ui_room.models.RoomDetailsUi
-import com.kerymov.ui_room.screens.RoomScreen
 import com.kerymov.ui_room.viewModels.RoomViewModel
 import com.kerymov.ui_rooms.screens.RoomsScreen
 import com.kerymov.ui_rooms.viewModels.RoomsViewModel
@@ -58,6 +60,8 @@ fun RootNavContainer(
     val currentBackStackEntry by navController.currentBackStackEntryAsState()
 
     val scaffoldState by rootViewModel.scaffoldState.collectAsState()
+
+    val isTopAppBarVisible = remember { mutableStateOf(true) }
     val topAppBarScrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
     Scaffold(
@@ -89,10 +93,12 @@ fun RootNavContainer(
         },
         topBar = {
             scaffoldState.topAppBar?.let {
-                CenterAlignedTopBar(
-                    item = it,
-                    scrollBehaviour = topAppBarScrollBehavior
-                )
+                if (isTopAppBarVisible.value) {
+                    CenterAlignedTopBar(
+                        item = it,
+                        scrollBehaviour = topAppBarScrollBehavior
+                    )
+                }
             }
         },
         containerColor = MaterialTheme.colorScheme.background,
@@ -166,7 +172,7 @@ fun RootNavContainer(
                             title = stringResource(R.string.rooms),
                             actions = listOf(
                                 TopAppBarInteractionItem(
-                                    icon = Icons.Filled.Add,
+                                    icon = Icons.Rounded.Add,
                                     onClick = { roomsViewModel.toggleCreateRoomBottomSheet(isOpen = true) }
                                 )
                             )
@@ -219,15 +225,17 @@ fun RootNavContainer(
                             item = TopAppBarItem(
                                 title = "${details.name} - ${details.settings.event.shortName}",
                                 navigationItem = TopAppBarInteractionItem(
-                                    icon = Icons.AutoMirrored.Filled.ExitToApp,
+                                    icon = Icons.AutoMirrored.Rounded.ExitToApp,
                                     onClick = {
                                         viewModel.toggleExitConfirmationDialog(true)
                                     }
                                 ),
                                 actions = listOf(
                                     TopAppBarInteractionItem(
-                                        icon = Icons.Filled.Groups,
-                                        onClick = { }
+                                        icon = Icons.Rounded.Timeline,
+                                        onClick = {
+                                            viewModel.toggleResultsSheet(true)
+                                        }
                                     )
                                 ),
                             )
@@ -240,6 +248,9 @@ fun RootNavContainer(
                                     navController.navigate(Main.Rooms) {
                                         popUpTo(0)
                                     }
+                                },
+                                onTopAppBarVisibilityChange = { isVisible ->
+                                    isTopAppBarVisible.value = isVisible
                                 },
                                 modifier = Modifier.fillMaxSize()
                             )
