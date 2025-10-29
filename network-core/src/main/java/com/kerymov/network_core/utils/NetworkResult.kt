e8774fc757876cd497a55d2abe14d1e7466892f1
illia kerymov <illya.kerymov31@gmail.com>
2025-10-29 03:28:38 +0100

    #51. Extend NetworkResult class with additional exceptions.

--- network-core/src/main/java/com/kerymov/network_core/utils/NetworkResult.kt
+++ network-core/src/main/java/com/kerymov/network_core/utils/NetworkResult.kt
@@ -2,11 +2,30 @@ package com.kerymov.network_core.utils
 
 import retrofit2.HttpException
 import retrofit2.Response
+import java.net.NoRouteToHostException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
+import java.security.cert.CertPathValidatorException
+import java.util.concurrent.TimeoutException
+import javax.net.ssl.SSLHandshakeException
 
 sealed class NetworkResult<T : Any> {
     class Success<T : Any>(val data: T) : NetworkResult<T>()
-    class Error<T : Any>(val code: Int, val message: String?) : NetworkResult<T>()
-    class Exception<T : Any>(val e: Throwable) : NetworkResult<T>()
+    sealed class Error<T : Any>(val code: Int, val message: String?) : NetworkResult<T>() {
+        data class ServerUnavailable<T : Any>(val response: Response<T>) : Error<T>(code = response.code(), message = response.message())
+        data class BadRequest<T : Any>(val response: Response<T>) : Error<T>(code = response.code(), message = response.message())
+        data class NotFound<T : Any>(val response: Response<T>) : Error<T>(code = response.code(), message = response.message())
+        data class Unauthorized<T : Any>(val response: Response<T>) : Error<T>(code = response.code(), message = response.message())
+        data class Forbidden<T : Any>(val response: Response<T>) : Error<T>(code = response.code(), message = response.message())
+        data class TooManyRequests<T : Any>(val response: Response<T>) : Error<T>(code = response.code(), message = response.message())
+        data class Unknown<T : Any>(val response: Response<T>) : Error<T>(code = response.code(), message = response.message())
+    }
+    sealed class Exception<T : Any>(val e: Throwable) : NetworkResult<T>() {
+        data class NoInternet<T : Any>(val exception: kotlin.Exception) : Exception<T>(e = exception)
+        data class Timeout<T : Any>(val exception: kotlin.Exception) : Exception<T>(e = exception)
+        data class SSL<T : Any>(val exception: kotlin.Exception) : Exception<T>(e = exception)
+        data class Unknown<T : Any>(val exception: kotlin.Exception) : Exception<T>(e = exception)
+    }
 }
 
 suspend fun <T:Any> handleApi(
@@ -17,10 +36,28 @@ suspend fun <T:Any> handleApi(
     if (response.isSuccessful && body != null) {
         NetworkResult.Success(body)
     } else {
-        NetworkResult.Error(code = response.code(), message = response.message())
+        when (response.code()) {
+            400 -> NetworkResult.Error.BadRequest(response)
+            401 -> NetworkResult.Error.Unauthorized(response)
+            403 -> NetworkResult.Error.Forbidden(response)
+            404 -> NetworkResult.Error.NotFound(response)
+            429 -> NetworkResult.Error.TooManyRequests(response)
+            in 500..599 -> NetworkResult.Error.ServerUnavailable(response)
+            else -> NetworkResult.Error.Unknown(response)
+        }
     }
+} catch (e: UnknownHostException) {
+    NetworkResult.Exception.NoInternet(e)
+} catch (e: NoRouteToHostException) {
+    NetworkResult.Exception.NoInternet(e)
+} catch (e: SocketTimeoutException) {
+    NetworkResult.Exception.Timeout(e)
+} catch (e: TimeoutException) {
+    NetworkResult.Exception.Timeout(e)
+} catch (e: SSLHandshakeException) {
+    NetworkResult.Exception.SSL(e)
+} catch (e: CertPathValidatorException) {
+    NetworkResult.Exception.SSL(e)
 } catch (e: HttpException) {
-    NetworkResult.Error(code = e.code(), message = e.message())
-} catch (e: Throwable) {
-    NetworkResult.Exception(e)
+    NetworkResult.Exception.Unknown(e)
 }
