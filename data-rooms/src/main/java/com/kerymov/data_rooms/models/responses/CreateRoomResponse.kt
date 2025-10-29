1c2e1403009ea9c126beebeff2fa290020df46ad
illia kerymov <illya.kerymov31@gmail.com>
2025-10-29 03:26:36 +0100

    #51. Mark some responses as 'always successful'.

--- data-rooms/src/main/java/com/kerymov/data_rooms/models/responses/CreateRoomResponse.kt
+++ data-rooms/src/main/java/com/kerymov/data_rooms/models/responses/CreateRoomResponse.kt
@@ -2,12 +2,13 @@ package com.kerymov.data_rooms.models.responses
 
 import com.kerymov.data_rooms.models.CreateRoomDetailsDto
 import com.google.gson.annotations.SerializedName
+import com.kerymov.network_core.AlwaysSuccessfulResponse
 
 data class CreateRoomResponse(
-    val isSuccess: Boolean,
-    val statusCode: Int,
-    val errorMessage: String?,
+    override val isSuccess: Boolean,
+    override val statusCode: Int,
+    override val errorMessage: String?,
 
     @SerializedName("model")
-    val roomDetails: CreateRoomDetailsDto,
-)
+    val roomDetails: CreateRoomDetailsDto
+) : AlwaysSuccessfulResponse
